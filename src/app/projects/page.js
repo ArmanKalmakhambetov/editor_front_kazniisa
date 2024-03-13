d7570e2663bf1e7e757ffbcb37d68ef73e693f24
KArman <arman458792@gmail.com>
2024-03-13 18:30:22 +0500

    first commit

---
+++ src/app/projects/page.js
@@ -0,0 +1,8 @@
+'use client'
+import UserProjects from "@/components/userprojects"
+//TODO:отображение всех проектов из базы с помощью селектора если нет данных из базы, то показывать что нет проектов и предложить создать новый проект
+export default function ProjectsPage() {
+  return (
+    <UserProjects/>
+  )
+}
---
+++ src/components/userprojects/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+//TODO: получаем с базы все проекты и мапим в компонент Project кидаем элемент проекта как пропс
+export default function UserProjects() {
+  return (
+    <div>UserProjects</div>
+  )
+}
